37b6fb03ad0b4931f497a93dcbf351cccb7b0f57
andrewgomescode <andrewgomes.brc@gmail.com>
2024-01-25 07:00:11 -0300

    c

--- moments/src/app/components/pages/home/home.component.ts
+++ moments/src/app/components/pages/home/home.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { environment } from './../../../../environments/environment.development';
+import { MomentService } from './../../../services/moment.service';
+import { Moment } from '../../../Moment';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  allMoments: Moment[] = [];
+  moments: Moment[] = [];
+  baseApiUrl = environment.baseApiUrl;
 
+  //todo search
+
+  constructor(private momentService: MomentService) {}
+
+  ngOnInit(): void {
+    this.momentService.getMoments().subscribe((items) => {
+      const data = items.data;
+
+      data.map((item) => {
+        item.created_at = new Date(item.created_at!).toLocaleDateString(
+          'pt-BR'
+        );
+      });
+
+      this.allMoments = data;
+      this.moments = data;
+    });
+  }
 }
--- moments/src/app/components/pages/new-moment/new-moment.component.ts
+++ moments/src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { MomentService } from './../../../services/moment.service';
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Moment } from '../../../Moment';
+import { MessagesService } from '../../../services/messages.service';
 
 @Component({
   selector: 'app-new-moment',
   templateUrl: './new-moment.component.html',
   styleUrl: './new-moment.component.css',
 })
-export class NewMomentComponent {
-  btnText = 'compartilhar!';
+export class NewMomentComponent implements OnInit {
+  btnText = 'Compartilhar!';
+
+  constructor(
+    private momentService: MomentService,
+    private router: Router,
+    private messagesService: MessagesService
+  ) {}
+
+  ngOnInit(): void {}
+
+  async createHandler(moment: Moment) {
+    const formData = new FormData();
+
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
+
+    if (moment.image) {
+      formData.append('image', moment.image);
+    }
+
+    await this.momentService.createMoment(formData).subscribe();
+
+    this.messagesService.add('Momento adicionado com sucesso!');
+
+    this.router.navigate(['/']);
+  }
 }
